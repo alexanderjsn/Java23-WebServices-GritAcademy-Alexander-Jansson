d9792e0abe2b060a50cb5a7b8bc42c7430730ae5
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-03-22 15:45:14 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/Java23WebServicesGritAcademyApiFornamnEfternamnApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java23WebServicesGritAcademyApiFornamnEfternamnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java23WebServicesGritAcademyApiFornamnEfternamnApplication.class, args);
+	}
+
+}
