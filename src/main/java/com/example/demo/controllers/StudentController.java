e40155cb620306ea97416cb825c0314101504724
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-03-22 15:58:25 +0100

    gjorde en controller och testade så dem ufnkade

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,4 +1,15 @@
 package com.example.demo.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 
 public class StudentController {
+    @GetMapping("/getStudents")
+    public String getStudents(){
+        return "test";
+
+    }
+
+
+
 }
